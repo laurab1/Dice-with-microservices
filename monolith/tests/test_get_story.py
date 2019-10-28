14348953d90a75ef8e4441ce80a354f03446ca05
Remo <andreoliremo@gmail.com>
2019-10-28 21:00:09 +0100

    added status code check in test

--- monolith/tests/test_get_story.py
+++ monolith/tests/test_get_story.py
@@ -15,16 +15,19 @@ class TestGetStory(unittest.TestCase):
         reply = app.get('/stories/1')
         data = str(reply.data, 'utf8')
         
-        self.assertEqual(data, '<html>\n  <body>\n    <h1>Story List</h1>\n    <h5></h5>\n    <ul>\n      \n      <li>\n      \"Trial story of example admin user :)\"    Likes: 42 (2019-10-24 12:22:20.016285)\n      \n      </li>\n      \n    </ul>\n  </body>\n</html>')
+        self.assertEqual(data, '<html>\n  <body>\n    <h1>Story List</h1>\n    <h5></h5>\n    <ul>\n      \n      <li>\n      \"Trial story of example admin user :)\"    Likes: 42 (2019-10-27 21:09:18.895015)\n      \n      </li>\n      \n    </ul>\n  </body>\n</html>')
+        self.assertEqual(reply.status_code, 200)
 
         #story not found
         reply = app.get('/stories/0')
         data = str(reply.data, 'utf8')
         
         self.assertEqual(data, '<html>\n  <body>\n    <h1>Story List</h1>\n    <h5>story not found!</h5>\n    <ul>\n      \n    </ul>\n  </body>\n</html>')
+        self.assertEqual(reply.status_code, 200)
 
         #invalid input
         reply = app.get('stories/ciao')
         data = str(reply.data, 'utf8')
         
         self.assertEqual(data, '<html>\n  <body>\n    <h1>Story List</h1>\n    <h5>story not found!</h5>\n    <ul>\n      \n    </ul>\n  </body>\n</html>')
+        self.assertEqual(reply.status_code, 200)
