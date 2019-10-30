bb15a4eda4ad84feaa4c4f091215d71e739a1272
Giuseppe Lomurno <lomurno.giuseppe97@gmail.com>
2019-10-30 18:05:03 +0100

    Follow/unfollow endpoint implemented.

--- monolith/views/users.py
+++ monolith/views/users.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, redirect, render_template, request, jsonify
-from flask_login import login_user
+from flask_login import (current_user, login_user, login_required)
 from monolith.database import db, User
 from monolith.auth import admin_required
 from monolith.forms import UserForm
@@ -8,6 +8,7 @@ from os import urandom
 
 
 users = Blueprint('users', __name__)
+
 
 @users.route('/users')
 def users_():
@@ -20,21 +21,54 @@ def users_():
 @users.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = UserForm()
-        
+
     if form.validate_on_submit():
         new_user = User()
         form.populate_obj(new_user)
         new_user.set_password(form.password.data)
         db.session.add(new_user)
-       
+
         try:
             db.session.commit()
             login_user(new_user)
             return redirect('/')
         except IntegrityError as e:
             if 'user.username' in str(e):
-                return jsonify({'Error':'This username already exists.'})
+                return jsonify({'Error': 'This username already exists.'})
             elif 'user.email' in str(e):
-                return jsonify({'Error':'This email is already used.'})
-        
+                return jsonify({'Error': 'This email is already used.'})
+
     return render_template('signup.html', form=form)
+
+
+@users.route('/users/<user_id>/follow', methods=['DELETE', 'POST'])
+@login_required
+def follow(user_id):
+    """POST requests add the user with primary key `user_id` to the list of
+    user followed by the currenly logged user. If it is already in the list
+    returns successfully without updating the database.
+    DELETE requests remove the user from the list and if it is not in the list
+    returns successfully without updating the database.
+    User must be logged to access this endpoint.
+    All responses are in JSON format and are meant to be invokated within the
+    the frontend (eg. AJAX or fetch), not by url access."""
+
+    followee = User.query.get(user_id)
+    if followee is None:
+        message = 'User with id {} does not exists'.format(user_id)
+        return (jsonify(error=message), 404)
+
+    if request.method == 'POST':
+        current_user.follows.append(followee)
+        db.session.commit()
+        return jsonify(message='User followed')
+
+    if request.method == 'DELETE':
+        try:
+            current_user.follows.remove(followee)
+            db.session.commit()
+        except ValueError:
+            pass
+        return jsonify(message='User unfollowed')
+
+    abort(405)
