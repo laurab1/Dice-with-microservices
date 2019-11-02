0c57d7dd9916e963bb5baa1b8149a76669ca3419
Laura <l.bussi1@studenti.unipi.it>
2019-11-02 17:58:04 +0100

    correct import

--- monolith/views/stories.py
+++ monolith/views/stories.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, redirect, render_template, request, abort, jsonify
-from monolith.database import db, Story, Like
+from monolith.database import db, Story, Reactions
 from flask_login import (current_user, login_user, logout_user, login_required)
 from flask import current_app as app
 from sqlalchemy import desc
