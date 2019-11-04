3a8c3fac3f7399c884f61e5e7131457e7c8a38b2
Giuseppe Lomurno <lomurno.giuseppe97@gmail.com>
2019-11-04 13:51:35 +0100

    Added draft and reworked new story input

--- monolith/tests/test_stories.py
+++ monolith/tests/test_stories.py
@@ -1,36 +1,133 @@
 from monolith.database import Story
 
 
-def test_write_new_story(client, auth, database, templates):
+def test_new_story(client, auth, database, templates):
     auth.login('Admin', 'admin')
 
-    client.get('/rollDice')
+    reply = client.get('/rollDice', follow_redirects=True)
+    assert reply.status_code == 200
+    new_id = templates[-1]['story_id']
+    story = database.session.query(Story).get(new_id)
+    assert story.text == ''
+    assert story.author_id == 1
+    assert story.is_draft
+    assert story.likes == 0
+    assert story.dislikes == 0
+
+
+def test_edit_story(client, auth, database, templates):
+    auth.login('Admin', 'admin')
+
+    reply = client.get('/rollDice', follow_redirects=True)
+    assert reply.status_code == 200
     roll = templates[-1]['dice']
+    new_id = templates[-1]['story_id']
 
     story_text = ''
     for i in range(len(roll)):
         story_text = story_text + roll[i] + ' '
 
-    reply = client.post('/writeStory', data={'text': story_text})
+    reply = client.post(f'/stories/{new_id}/edit', data={'text': story_text})
     assert reply.status_code == 302
 
     story = database.session.query(Story).order_by(Story.date.desc()).first()
     assert story.text == story_text
     assert story.author_id == 1
 
-    reply = client.post('/writeStory', data={})
+    reply = client.get('/rollDice', follow_redirects=True)
+    assert reply.status_code == 200
+    roll = templates[-1]['dice']
+    new_id = templates[-1]['story_id']
+    reply = client.post(f'/stories/{new_id}/edit', data={})
     assert reply.status_code == 400
 
 
-def test_write_new_not_valid_story(client, auth, database, templates):
+def test_edit_non_draft_story(client, auth, database, templates):
     auth.login('Admin', 'admin')
 
-    client.get('/rollDice')
+    reply = client.get('/rollDice', follow_redirects=True)
+    assert reply.status_code == 200
+    new_id = templates[-1]['story_id']
     roll = templates[-1]['dice']
+
+    story_text = ''
+    for i in range(len(roll)):
+        story_text = story_text + roll[i] + ' '
+
+    reply = client.post(f'/stories/{new_id}/edit',
+                        data={'text': story_text})
+    assert reply.status_code == 302
+    story = database.session.query(Story).get(new_id)
+    assert not story.is_draft
+
+    reply = client.post(f'/stories/{new_id}/edit', data={'text': story_text})
+    assert reply.status_code == 403
+
+
+def test_edit_not_author_story(client, auth, database, templates):
+    auth.login('Admin', 'admin')
+    reply = client.get('/rollDice', follow_redirects=True)
+    assert reply.status_code == 200
+    new_id = templates[-1]['story_id']
+    auth.logout()
+
+    auth.login('test1', 'test1123')
+    reply = client.post(f'/stories/{new_id}/edit',
+                        data={'text': ''})
+    assert reply.status_code == 401
+
+
+def test_edit_non_existent_story(client, auth, database):
+    auth.login('Admin', 'admin')
+    reply = client.get('/stories/0/edit')
+    assert reply.status_code == 404
+
+
+def test_edit_not_valid_story(client, auth, database, templates):
+    auth.login('Admin', 'admin')
+
+    reply = client.get('/rollDice', follow_redirects=True)
+    assert reply.status_code == 200
+    roll = templates[-1]['dice']
+    new_id = templates[-1]['story_id']
 
     story = ''
     for i in range(1, len(roll)):
         story = story + roll[i] + ' '
 
-    reply = client.post('/writeStory', data={'text': story})
+    reply = client.post(f'/stories/{new_id}/edit',
+                        data={'text': story})
+    database.session.commit()
     assert reply.status_code == 400
+
+
+def test_edit_draft_valid_story(client, auth, database, templates):
+    auth.login('Admin', 'admin')
+
+    reply = client.get('/rollDice', follow_redirects=True)
+    assert reply.status_code == 200
+    new_id = templates[-1]['story_id']
+
+    reply = client.post(f'/stories/{new_id}/edit',
+                        data={'text': 'blob', 'is_draft': 'y'})
+    database.session.commit()
+    assert reply.status_code == 302
+
+
+def test_edit_get(client, auth, database, templates):
+    auth.login('Admin', 'admin')
+
+    reply = client.get('/rollDice', follow_redirects=True)
+    assert reply.status_code == 200
+    roll = templates[-1]['dice']
+    new_id = templates[-1]['story_id']
+
+    story_text = ''
+    for i in range(len(roll)):
+        story_text = story_text + roll[i] + ' '
+
+    reply = client.get(f'/stories/{new_id}/edit')
+    assert reply.status_code == 200
+    template_capture = templates[-1]
+    assert template_capture['story_id'] == new_id
+    assert template_capture['dice'] == roll
--- monolith/tests/test_valid_story.py
+++ monolith/tests/test_valid_story.py
@@ -1,9 +1,9 @@
+from monolith.classes.DiceSet import DiceSet
+from monolith.utility.diceutils import *
+from monolith.utility.validate_story import NotValidStoryError, _check_story
+
 import pytest
 
-from monolith.utility.diceutils import *
-from monolith import app as test_app
-from monolith.utility.validate_story import _check_story,NotValidStoryError,get_synonyms
-from monolith.classes.DiceSet import DiceSet
 
 def test_correct_story():
     diceset = 'standard'
@@ -15,10 +15,8 @@ def test_correct_story():
     for word in roll:
         story += word + ' '
 
-    try:
-        _check_story(roll,story)
-    except NotValidStoryError:
-        raise NotValidStoryError('Test failed...')
+    _check_story(roll, story)
+
 
 def test_empty_story():
     diceset = 'standard'
@@ -34,28 +32,19 @@ def test_empty_story():
 def test_correct_story_with_punctuation():
     roll = ['table', 'window', 'cat', 'chair']
     story = 'The cat is on the table! The chair, I think, is near the window.'
-    try:
-        _check_story(roll, story)
-    except NotValidStoryError:
-        raise NotValidStoryError("Test failed...")
+    _check_story(roll, story)
 
 
 def test_correct_story_with_parentesys():
     roll = ['table', 'window', 'cat', 'chair']
     story = 'The cat is on the table and...the chair, I think, is near the window:)'
-    try:
-        _check_story(roll,story)
-    except NotValidStoryError:
-        raise NotValidStoryError("Test failed...")
+    _check_story(roll, story)
 
 
 def test_correct_story_synonym():
     roll = ['table', 'window', 'cat', 'chair']
     story = 'The cat is on the board, the chair is near the window.'
-    try:
-        _check_story(roll,story)
-    except NotValidStoryError:
-        raise NotValidStoryError("Test failed...")
+    _check_story(roll, story)
 
 
 def test_wrong_story_synonym():
@@ -68,7 +57,5 @@ def test_wrong_story_synonym():
 def test_wrong_word_in_roll():
     roll = ['table', 'window', 'cat', 'chai']
     story = 'The cat is on the board, the chair is near the window.'
-    try:
+    with pytest.raises(NotValidStoryError):
         _check_story(roll, story)
-    except NotValidStoryError as error:
-        print(str(error))
