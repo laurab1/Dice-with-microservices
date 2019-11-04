b94038719ee8a91e0a1885d1e30f7ac27f16989a
Marco <cardiamc@gmail.com>
2019-11-04 17:18:44 +0100

    Nav bar rivisited. Created new statistics.

--- monolith/views/home.py
+++ monolith/views/home.py
@@ -7,23 +7,30 @@ from monolith.database import Story, db
 
 home = Blueprint('home', __name__)
 
-
-def _strava_auth_url(config):
-    return '127.0.0.1:5000'
-
-
 @home.route('/')
 def index():
+    stats = {}
     if current_user is not None and hasattr(current_user, 'id'):
         stories = db.session.query(Story).filter(
             Story.author_id == current_user.id).all()
+            
 
         likes = db.session.query(
                 db.func.sum(Story.likes).label('total'),
-                db.func.count(Story.likes).label('number_of_stories')
+                db.func.count(Story.likes).label('number_of_stories'),
+                db.func.sum(Story.dislikes).label('total_dislikes')
             ).filter(
                 Story.author_id == current_user.id
-            ).group_by(Story.author_id)
+            ).group_by(Story.author_id).all()
+
+        stats['likes'] = likes[0]
+        
+        print(stats['likes'].total)
+        if stories != []:
+            n_dice = [len(story.dice_set.split('?')) for story in stories]
+            avg_dice = round(sum(n_dice) / len(n_dice), 2)
+        
+        stats['avg_dice'] = avg_dice
 
         if app.config['TESTING']:
             return jsonify({'stories':[s.toJSON() for s in stories]})
@@ -32,4 +39,4 @@ def index():
         if app.config['TESTING']:
             return jsonify(login='needed')
 
-    return render_template('index.html', stories=stories, likes=likes)
+    return render_template('index.html', stories=stories, stats=stats)
