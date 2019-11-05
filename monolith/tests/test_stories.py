6396de94ed50a03d9b0c83ef556f48f449b23f02
Alessandro <alessandrodgr@gmail.com>
2019-11-05 18:36:32 +0100

    Delete story testing refactored

--- monolith/tests/test_stories.py
+++ monolith/tests/test_stories.py
@@ -143,12 +143,98 @@ def test_edit_get(client, auth, database, templates):
     assert template_capture['story_id'] == new_id
     assert template_capture['dice'] == roll
 
+def test_empty_story_list(client, auth, database, templates):
+    reply = client.get('/stories')
+    assert reply.status_code == 200
+    stories = templates[-1]['stories']
+    assert stories.count() == 0 # no loaded stories
 
-def test_delete_story(client, auth, database, templates):
+def test_nonempty_story_list(client, auth, database, templates):
+    auth.login()
+
+    # add 1 story
+    reply = client.get('/roll_dice', follow_redirects=True)
+    assert reply.status_code == 200
+    roll = templates[-1]['dice']
+    new_id = templates[-1]['story_id']
+
+    story_text = ''
+    for i in range(len(roll)):
+        story_text = story_text + roll[i] + ' '
+
+    reply = client.post(f'/stories/{new_id}/edit', data={'text': story_text})
+    assert reply.status_code == 302
+
+    #check only 1 story is present
+    reply = client.get('/stories')
+    assert reply.status_code == 200
+    stories = templates[-1]['stories']
+    assert stories.count() == 1
+    #and it is the one just published
+    query = database.session.query(Story).filter_by(deleted=False).order_by(desc(Story.date))
+    assert query.count() == stories.count()
+    assert query[0].id == stories[0].id
+    assert query[0].text == stories[0].text
+    assert query[0].author_id == stories[0].author_id
+
+    #add another story
+    reply = client.get('/roll_dice', follow_redirects=True)
+    assert reply.status_code == 200
+    roll = templates[-1]['dice']
+    new_id = templates[-1]['story_id']
+
+    story_text = ''
+    for i in range(len(roll)):
+        story_text = story_text + roll[i] + ' '
+
+    reply = client.post(f'/stories/{new_id}/edit', data={'text': story_text})
+    assert reply.status_code == 302
+
+    #check all stories are present
+    reply = client.get('/stories')
+    assert reply.status_code == 200
+    stories = templates[-1]['stories']
+    assert stories.count() == 2
+
+    #check that queried and rendered items are the same
+    query = database.session.query(Story).filter_by(deleted=False).order_by(desc(Story.date))
+
+    assert stories.count() == query.count()
+
+    for i in range(stories.count()):
+        assert query[i].id == stories[i].id
+        assert query[i].text == stories[i].text
+
+def test_delete_nonexisting_story(client, auth, database, templates):
     auth.login()
 
     reply = client.delete('/stories/0') # delete non-existing story
     assert reply.status_code == 404
+
+def test_delete_existing_story(client, auth, database, templates):
+    auth.login()
+
+    # new story
+    reply = client.get('/roll_dice', follow_redirects=True)
+    assert reply.status_code == 200
+    roll = templates[-1]['dice']
+    new_id = templates[-1]['story_id']
+
+    story_text = ''
+    for i in range(len(roll)):
+        story_text = story_text + roll[i] + ' '
+
+    reply = client.post(f'/stories/{new_id}/edit', data={'text': story_text})
+    assert reply.status_code == 302
+
+    # delete the newly created story
+    reply = client.delete(f'/stories/{new_id}')
+    assert reply.status_code == 200
+    deletedStory = database.session.query(Story).get(new_id)
+    assert deletedStory.deleted
+
+def test_delete_already_deleted_story(client, auth, database, templates):
+    auth.login()
 
     # new story
     reply = client.get('/roll_dice', follow_redirects=True)
@@ -177,8 +263,7 @@ def test_delete_story(client, auth, database, templates):
     reply = client.delete(f'/stories/{new_id}')
     assert reply.status_code == 400
 
-    auth.logout()
-
+def test_delete_story_of_another_user(client, auth, database, templates):
     auth.login('test1', 'test1123')
     # new story by user test1
     reply = client.get('/roll_dice', follow_redirects=True)
@@ -194,88 +279,8 @@ def test_delete_story(client, auth, database, templates):
     assert reply.status_code == 302
     auth.logout()
 
-    auth.login()
+    auth.login('test2', 'test2123')
 
     # delete story of another user
     reply = client.delete(f'/stories/{new_id}')
     assert reply.status_code == 403
-
-def test_see_all_stories(client, auth, database, templates):
-    reply = client.get('/stories')
-    assert reply.status_code == 200
-    stories = templates[-1]['stories']
-    assert stories.count() == 0 # no loaded stories
-
-    auth.login('Admin', 'admin')
-
-    # add 2 stories
-    reply = client.get('/roll_dice', follow_redirects=True)
-    assert reply.status_code == 200
-    roll = templates[-1]['dice']
-    new_id = templates[-1]['story_id']
-
-    story_text = ''
-    for i in range(len(roll)):
-        story_text = story_text + roll[i] + ' '
-
-    reply = client.post(f'/stories/{new_id}/edit', data={'text': story_text})
-    assert reply.status_code == 302
-
-    reply = client.get('/roll_dice', follow_redirects=True)
-    assert reply.status_code == 200
-    roll = templates[-1]['dice']
-    new_id = templates[-1]['story_id']
-
-    story_text = ''
-    for i in range(len(roll)):
-        story_text = story_text + roll[i] + ' '
-
-    reply = client.post(f'/stories/{new_id}/edit', data={'text': story_text})
-    assert reply.status_code == 302
-
-    #check newly added stories
-    reply = client.get('/stories')
-    assert reply.status_code == 200
-    stories = templates[-1]['stories']
-    assert stories.count() == 2
-
-    #check that queried and rendered items are the same
-    query = database.session.query(Story).filter_by(deleted=False).order_by(desc(Story.date))
-
-    assert stories.count() == query.count()
-
-    for i in range(stories.count()):
-        assert query[i].id == stories[i].id
-        assert query[i].text == stories[i].text
-
-    # remove a story
-    reply = client.delete(f'/stories/{stories[0].id}')
-    assert reply.status_code == 200
-
-    #check stories after deletion
-    reply = client.get('/stories')
-    assert reply.status_code == 200
-    stories = templates[-1]['stories']
-    assert stories.count() == 1
-
-    query = database.session.query(Story).filter_by(deleted=False).order_by(desc(Story.date))
-
-    assert stories.count() == query.count()
-
-    for i in range(stories.count()):
-        assert query[i].id == stories[i].id
-        assert query[i].text == stories[i].text
-
-    #delete remaining story
-    reply = client.delete(f'/stories/{stories[0].id}')
-    assert reply.status_code == 200
-
-    #check there are no more stories
-    reply = client.get('/stories')
-    assert reply.status_code == 200
-    stories = templates[-1]['stories']
-    assert stories.count() == 0
-
-    query = database.session.query(Story).filter_by(deleted=False).order_by(desc(Story.date))
-
-    assert stories.count() == query.count()
