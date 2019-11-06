5b945e93f8c5a03871d4e3ce40d52fa8ed517044
Alessandro <alessandrodgr@gmail.com>
2019-11-06 17:12:52 +0100

    Added missing tests for unmarked reactions

--- monolith/tests/test_get_story.py
+++ monolith/tests/test_get_story.py
@@ -1,4 +1,4 @@
-from monolith.database import Story
+from monolith.database import Story, Reaction
 
 
 def test_get_story(client, auth, database, templates):
@@ -44,3 +44,61 @@ def test_unauthorized_draft(client, auth, database, templates):
     auth.login('test1', 'test1123')
     reply = client.get(f'/stories/{new_id}')
     assert reply.status_code == 403
+
+def test_story_with_unmarked_like(client, auth, database, templates):
+    s = Story()
+    s.text = 'Trial story of example admin user :)'
+    s.likes = 42
+    s.dislikes = 0
+    s.author_id = 1
+    s.dice_set = ['dice1', 'dice2']
+
+    database.session.add(s)
+
+    r = Reaction()
+    r.reactor_id = 1
+    r.author = s
+    r.reaction_val = 1
+    r.marked = False
+
+    database.session.add(r)
+
+    database.session.commit()
+
+    auth.login()
+    reply = client.get('/stories/1')
+    template_capture = templates[-1]
+    assert reply.status_code == 200
+    assert template_capture['story'].likes == 43
+    assert template_capture['story'].dislikes == 0
+
+    database.session.commit()
+
+def test_story_with_unmarked_dislike(client, auth, database, templates):
+    s = Story()
+    s.text = 'Trial story of example admin user :)'
+    s.likes = 42
+    s.dislikes = 0
+    s.author_id = 1
+    s.dice_set = ['dice1', 'dice2']
+
+    database.session.add(s)
+
+    r = Reaction()
+    r.reactor_id = 1
+    r.author = s
+    r.reaction_val = -1
+    r.marked = False
+
+    database.session.add(r)
+
+    database.session.commit()
+
+    auth.login()
+    reply = client.get('/stories/1')
+    template_capture = templates[-1]
+    assert reply.status_code == 200
+    assert template_capture['story'].likes == 42
+    assert template_capture['story'].dislikes == 1
+
+    database.session.commit()
