b3546e580b414dc58956dfbc2864d93887c0233c
Remo <andreoliremo@gmail.com>
2019-11-07 23:27:05 +0100

    finished testing interactions with stories and reactions using two users

--- monolith/tests/test_system.py
+++ monolith/tests/test_system.py
@@ -2,11 +2,13 @@ from monolith.database import Story
 from monolith.tests.conftest import get_auth, get_story_actions
 
 
-def test_draft_interaction(client_factory, templates):
+def test_users_interactions_with_stories_and_reactions(client_factory, templates):
+    #user1
     email1 = "prova@prova.com"
     username1 = "Test1"
     password1 = "12345678"
 
+    #user2
     email2 = "xx@xx.com"
     username2 = "Test2"
     password2 = "12345678"
@@ -54,93 +56,56 @@ def test_draft_interaction(client_factory, templates):
     stories = templates[-1]['stories']
     assert stories.count() == 1
 
-
-    
-'''
-def test_two_users(auth, story_actions, templates):
-    email = "prova@prova.com"
-    username = "Test"
-    password = "12345678"
-
-    # signup and login
-    reply = auth.signup(email=email, username=username, password=password)
-    assert reply.status_code == 302
-
-    reply = auth.login(username=username, password=password)
-    assert reply.status_code == 302
-
-    # add story (roll + edit)
-    reply = story_actions.roll_dice()
-    id = templates[-1]['story_id']
-    dices = templates[-1]['dice']
+    #client2 likes the story
+    reply = story_actions2.post_like_reaction(id)
     assert reply.status_code == 200
 
-    reply = story_actions.add_story_text(id, dices)
-    assert reply.status_code == 302
-
-    # check that I actually created a story
-    story_actions.get_all_stories()
+    #client2 visualizes if the story is correctly liked
+    story_actions2.get_all_stories()
     stories = templates[-1]['stories']
     assert stories.count() == 1
-    assert stories.first().id == int(id)
+    assert stories.first().likes == 1 and stories.first().dislikes == 0
 
-    # visualize story
-    reply = story_actions.get_story(id)
+    #client1 dislikes its own story
+    reply = story_actions1.post_dislike_reaction(id)
     assert reply.status_code == 200
 
-    # add draft
-    reply = story_actions.roll_dice()
-    id = templates[-1]['story_id']
-    dices = templates[-1]['dice']
+    #client1 visualizes if the story is correctly disliked
+    reply = story_actions1.get_all_stories()
+    stories = templates[-1]['stories']
+    assert stories.count() == 1
+    assert stories.first().likes == 1 and stories.first().dislikes == 1
+
+    #client2 create a draft for an xmas story
+    reply = story_actions2.roll_dice(dice_set="xmas")
+    xmas_id = templates[-1]['story_id']
+    xmas_dices = templates[-1]['dice']
     assert reply.status_code == 200
 
-    # log out
-    reply = auth.logout()
-    assert reply.status_code == 302
-
-    # log again
-    reply = auth.login(username=username, password=password)
-    assert reply.status_code == 302
-
-    # try to post an invalid story
-    reply = story_actions.add_story_text(id, dices, "text is wrong")
+    #client2 writes a wrong story
+    reply = story_actions2.add_story_text(xmas_id, dices, "text is wrong")
     assert reply.status_code == 200
 
-    # check on form
     form = templates[-1]['form']
     assert form.text.errors[-1] == 'The story is not valid.'
 
-    # check that doesn't show up in story list
-    story_actions.get_all_stories()
+    #client2 gives to client1 the id for the xmas story, thinking that it is viewable
+    reply = story_actions1.get_story(xmas_id)
+    assert reply.status_code == 403
+
+    #client1 fixes the error, by correctly posting the story
+    reply = story_actions2.add_story_text(xmas_id, xmas_dices)
+    assert reply.status_code == 302
+
+    #client1 searches by interest
+    reply = story_actions1.get_all_stories(theme='xmas')
     stories = templates[-1]['stories']
     assert stories.count() == 1
 
-    # try to sign up when already logged
-    reply = auth.signup(email=email, username=username, password=password)
+    #client1 logout
+    reply = auth1.logout()
     assert reply.status_code == 302
 
-    # correct signup and login
-    other_email = "aa@bb.com"
-    other_username = "Test2"
-    other_password = "12345678"
-
-    reply = auth.logout()
+    #client2 logout
+    reply = auth1.logout()
     assert reply.status_code == 302
-
-    reply = auth.signup(email=other_email,
-                        username=other_username, password=other_password)
-    assert reply.status_code == 302
-
-    reply = auth.login(username=username, password=password)
-    assert reply.status_code == 302
-    assert templates[-1]['request'].path == '/stories'
-
-    # try to validate the draft of the other user (error!)
-    reply = story_actions.add_story_text(id, dices)
-    assert reply.status_code == 401
-
-    # check that doesn't show up in story list
-    story_actions.get_all_stories()
-    stories = templates[-1]['stories']
-    assert stories.count() == 1
-'''
